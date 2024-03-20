f1b91010507ea94ff596b3025335eefa96867c1e
poneding <poneding@gmail.com>
2024-03-20 16:42:35 +0800

    .

--- 04-MavenDemo/src/main/java/com/poneding/java/App.java
+++ 04-MavenDemo/src/main/java/com/poneding/java/App.java
@@ -6,8 +6,22 @@ package com.poneding.java;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    // public static void main( String[] args )
+    // {
+    //     System.out.println( "Hello World!" );
+    // }
+
+    public static void main(String[] args) {
+        int num1 = 48;
+        int num2 = 60;
+
+        int result = gcd(num1, num2);
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + result);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
 }
